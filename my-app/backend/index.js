0292635160fec2ffa86bb03d2e68a439c424cca6
shubham kate <shubhamkate962@gmail.com>
2024-11-16 10:58:58 +0530

    changgg

--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -143,7 +143,7 @@ const User = require("./models/ER-Module");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = ['https://new-repo-bice.vercel.app', 'http://localhost:5173'];
+const allowedOrigins = ['https://new-repo-bice.vercel.app', 'http://localhost:3000'];
 
 
 app.use(cors({
